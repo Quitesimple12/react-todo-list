4399b21f727a016a80f5c1573d6cb20f2e8e1bcc
Quitesimple12 <122361337+Quitesimple12@users.noreply.github.com>
2024-03-07 01:02:20 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
 const Home = () => {
+	const [inputvalue, setinputvalue] = useState('');
+	const [listitem, addlistitem] = useState([])
+
+	const addItemToList = (event) => {if (event.key === 'Enter') {
+		addlistitem(listitem.concat([inputvalue]))
+		setinputvalue('')
+	}}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>TODOS</h1>
+			<ul>
+				<li><input type="text"  
+					value={inputvalue} 
+					onChange= {(event) => setinputvalue(event.target.value) && addlistitem } 
+					onKeyPress = {addItemToList} 
+					placeholder="What are you going to do?"></input></li>
+				{listitem.map((item, index) => 
+				(<li>{item} <i class = "fas fa-trash-alt" 
+				onClick={() => 
+				addlistitem(
+					listitem.filter((t, currentIndex) => index != currentIndex))}></i></li>))}
+			</ul>
+			200 tasks
 		</div>
 	);
-};
+	};
 
 export default Home;
